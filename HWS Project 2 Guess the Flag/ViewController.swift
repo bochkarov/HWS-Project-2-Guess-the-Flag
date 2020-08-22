7f8ac023d64b4b192300c6e6d19b37cd9e228d14
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-08-22 17:40:11 +0200

    Initial Commit

---
+++ HWS Project 2 Guess the Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS Project 2 Guess the Flag
+//
+//  Created by Bochkarov Valentyn on 22/08/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
